74013f8740d850e3c9f0d47ed8cdb3ec1a6da629
Tyler Johnson <tejohnso@rogers.com>
2012-04-26 23:47:12 -0400

    added features

--- mus/compiler.js
+++ mus/compiler.js
@@ -5,12 +5,17 @@ var compile=function (MUS) {
     
     var recurse=function (MUS,parentSeqOrPar) {
         if (MUS.tag === 'note') {
-            compiledSong.push({tag: 'note', pitch: MUS.pitch,
+            compiledSong.push({tag: MUS.tag, pitch: MUS.pitch,
             start: startCount, dur: MUS.dur});
             if (parentSeqOrPar === 'par' && MUS.dur > harmonyLength) {
                 harmonyLength = MUS.dur;
             }
-            
+        }else if (MUS.tag === 'rest') {
+            compiledSong.push({tag: MUS.tag, start: startCount, dur: MUS.duration});            
+        }else if (MUS.tag === 'repeat') {
+            for (j=0,k=MUS.count;j<k;j++){
+                recurse(MUS.section,MUS.tag);
+            }
         }else{
             harmonyLength=0;
             recurse(MUS.left,MUS.tag);
@@ -24,6 +29,13 @@ var compile=function (MUS) {
     recurse(MUS);
     return compiledSong;
 };
+
+var convertPitch = function (pitch) {
+    var letterPitch={C: 0, D: 2, E: 4, F: 5, G: 7, A: 9, B: 11};
+    return 12 + 12 * parseInt(pitch.substr(1,1)) + letterPitch[pitch.substr(0,1)];
+}
+
+console.log(convertPitch('C4'));
 
 var melody_mus = 
     { tag: 'seq',
